760db49f8cf63324b55c0b1a8e62c0f80b0c7064
Alexander Bedoya Espinel <alex22122020@gmail.com>
2021-07-08 21:14:14 -0500

    Agrego correcciones

--- archivo.js
+++ archivo.js
@@ -2,10 +2,12 @@ class Articulos{
     constructor(nombre, precio){
         this.nombre = nombre;
         this.precio = parseFloat(precio);
-    }
-
-    hablar(){
-        alert("el producto es: "+ this.nombre + " y su precio es: " + this.precio);
+        this.producto = producto;
     }
 }
+
+const nombre = prompt("ingrese un nombre");
+const precio = number(prompt("ingrese su precio"));
+
+
 
